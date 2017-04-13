8d7634840e9668c7c7820f81167adf574e4ec0ca
Dmytro Vorobiov <d@dvor.me>
2017-04-13 07:43:19 +0200

    Accepting incoming friend request and incoming call

---
+++ TestingObjcToxVideo/TestingObjcToxVideo-Bridging-Header.h
@@ -0,0 +1,21 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <objcTox/OCTManager.h>
+#import <objcTox/OCTManagerConfiguration.h>
+#import <objcTox/OCTManagerFactory.h>
+#import <objcTox/OCTSubmanagerCalls.h>
+#import <objcTox/OCTSubmanagerCallsDelegate.h>
+#import <objcTox/OCTSubmanagerBootstrap.h>
+#import <objcTox/OCTSubmanagerUser.h>
+#import <objcTox/OCTSubmanagerObjects.h>
+#import <objcTox/OCTSubmanagerFriends.h>
+#import <objcTox/OCTFriendRequest.h>
+#import <objcTox/OCTCall.h>
+
+#undef LOG_INFO
+#undef LOG_DEBUG
+#import "DDLog.h"
+#import "DDASLLogger.h"
+#import "DDTTYLogger.h"
--- TestingObjcToxVideo/ViewController.swift
+++ TestingObjcToxVideo/ViewController.swift
@@ -9,17 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var toxManager: OCTManager!
+    var requestsToken: RLMNotificationToken?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        DDLog.add(DDASLLogger.sharedInstance())
+        DDLog.add(DDTTYLogger.sharedInstance())
+
+        let configuration = OCTManagerConfiguration.default()
+        OCTManagerFactory.manager(with: configuration, encryptPassword: "123", successBlock: { [weak self] manager in
+            self?.toxManager = manager
+            self?.didCreateManager()
+        }) { error in
+            print(error)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func didCreateManager() {
+        print("Tox ID: " + toxManager.user.userAddress)
+
+        toxManager.bootstrap.addPredefinedNodes()
+        toxManager.bootstrap.bootstrap()
+        toxManager.calls.delegate = self
+
+        autoApproveFriendRequests()
     }
 
+    func autoApproveFriendRequests() {
+        let results = toxManager.objects.objects(for: .friendRequest, predicate: nil)
+        requestsToken = results?.addNotificationBlock({ [weak self] (results, change, error) in
+            guard let results = results else {
+                return
+            }
 
+            for index in 0..<results.count {
+                guard let request = results[UInt(index)] as? OCTFriendRequest else {
+                    continue
+                }
+
+                _ = try? self?.toxManager.friends.approve(request)
+            }
+        })
+    }
 }
 
+extension ViewController: OCTSubmanagerCallDelegate {
+    func callSubmanager(_ callSubmanager: OCTSubmanagerCalls!, receive call: OCTCall!, audioEnabled: Bool, videoEnabled: Bool) {
+        _ = try? toxManager.calls.answer(call, enableAudio: true, enableVideo: true)
+    }
+}
